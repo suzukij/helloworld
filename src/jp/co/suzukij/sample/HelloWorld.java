dbba1520e85709749968a20f608ba20661ea318e
junya <junya@suzuki-PC>
2011-05-24 03:22:16 +0900

    initial commit.

---
+++ src/jp/co/suzukij/sample/HelloWorld.java
@@ -0,0 +1,12 @@
+package jp.co.suzukij.sample;
+
+public class HelloWorld {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
